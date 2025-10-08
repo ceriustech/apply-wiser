635258564e2e828e5a1f0df35c7e7036b09badcd
Bilal Masters <ceriustech@gmail.com>
2025-10-08 16:04:25 -0500

    feat: add folder to hold all site meta data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,10 @@
 import type { Route } from './+types/home';
+
 import { Welcome } from '../welcome/welcome';
+import { HOME_META_DATA } from '~/site_header_data/meta_data';
 
 export function meta({}: Route.MetaArgs) {
-	return [
-		{ title: 'Home | ApplyWiser' },
-		{ name: 'description', content: 'AI for resume tailoring' },
-	];
+	return HOME_META_DATA;
 }
 
 export default function Home() {
---
+++ app/site_header_data/meta_data.ts
@@ -0,0 +1,25 @@
+type HomeMetaDescriptor =
+	| { title: string }
+	| { name: string; content: string }
+	| { property: string; content: string }
+	| { 'http-equiv': string; content: string };
+
+const HOME_META_DATA: HomeMetaDescriptor[] = [
+	{ 'http-equiv': 'Content-type', content: 'text/html; charset=utf-8' },
+	{
+		name: 'viewport',
+		content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0',
+	},
+	{ title: 'ApplyWiser' },
+	{ name: 'description', content: 'AI for resume tailoring' },
+	{
+		name: 'keywords',
+		content:
+			'AI resume builder, resume tailoring, job application optimization, AI for job search, resume matching, ATS resume, resume analyzer, job-specific resume, CV optimizer, smart resume tool',
+	},
+	{ property: 'og:url', content: 'https://applywiser.app' },
+	{ property: 'og:title', content: 'Apply Wiser' },
+	{ property: 'og:description', content: 'AI resume helper' },
+];
+
+export { HOME_META_DATA };
