b833d927a96798c97977b17bec775957a0d72ad4
Bilal Masters <ceriustech@gmail.com>
2025-10-11 21:28:25 -0500

    fix: spelling error

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 			<section className="main-section">
 				<div className="page-heading">
 					<h1>Track Your Applications & resume Ratings</h1>
-					<h2>Review your submissions and check AI poered feedback.</h2>
+					<h2>Review your submissions and check AI powered feedback.</h2>
 				</div>
 			</section>
 		</main>
