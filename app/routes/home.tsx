b440ebeb7bac6fb44c52d5dd4535b4ace232e304
Bilal Masters <ceriustech@gmail.com>
2026-04-13 22:24:24 -0500

    feat: repo rewrite

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: 'ApplyWiser' },
+		{ name: 'description', content: 'Welcome to ApplyWiser!' },
+	];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return <div>Hello</div>;
 }
